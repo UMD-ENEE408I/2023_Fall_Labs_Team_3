9ddbdc3e2521a25073b7c06982709cccd17fb953
ShenzhiS <s.shenzhi42@gmail.com>
2023-09-04 19:29:39 -0400

    imu

--- Tutorial_1/IMU/src/main.cpp
+++ Tutorial_1/IMU/src/main.cpp
@@ -9,19 +9,135 @@ Adafruit_MPU6050 mpu;
 const unsigned int ADC_1_CS = 2;
 const unsigned int ADC_2_CS = 17;
 
-void setup(void) {
-  // Stop the right motor by setting pin 14 low
-  // this pin floats high or is pulled
-  // high during the bootloader phase for some reason
-  pinMode(14, OUTPUT);
-  digitalWrite(14, LOW);
-  delay(100);
-  
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+//motor 
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
 
-  Serial.println("Adafruit MPU6050 test!");
+const unsigned int M1_IN_1_CHANNEL = 0;
+const unsigned int M1_IN_2_CHANNEL = 1;
+const unsigned int M2_IN_1_CHANNEL = 2;
+const unsigned int M2_IN_2_CHANNEL = 3;
+
+const unsigned int M1_I_SENSE = 35;
+const unsigned int M2_I_SENSE = 34;
+
+const unsigned int PWM_VALUE = 512; // Do not give max PWM. Robot will move fast
+
+const int freq = 5000;
+const int resolution = 8;
+
+//motor functions
+void setLeftMotor(float value){
+  if (value == 0){
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+    return;
+  }
+  if (value > 0){
+    if (value > 255.0){
+      value = 255.0;
+    }
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, (uint32_t) value);
+  }
+  if (value < 0){
+    if (value < -255.0){
+      value = -255.0;
+    }
+    ledcWrite(M1_IN_1_CHANNEL, (uint32_t) -value);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+  }
+}
+
+void setRightMotor(float value){
+  if (value == 0){
+    ledcWrite(M2_IN_1_CHANNEL, 0);
+    ledcWrite(M2_IN_2_CHANNEL, 0);
+    return;
+  }
+  if (value > 0){
+    if (value > 255.0){
+      value = 255.0;
+    }
+    ledcWrite(M2_IN_1_CHANNEL, 0);
+    ledcWrite(M2_IN_2_CHANNEL, (uint32_t) value);
+  }
+  if (value < 0){
+    if (value < -255.0){
+      value = -255.0;
+    }
+    ledcWrite(M2_IN_1_CHANNEL, (uint32_t) -value);
+    ledcWrite(M2_IN_2_CHANNEL, 0);
+  }
+}
+
+//rotate
+//negative target degrees = clockwise from top down
+void rotate(float target_degrees){
+  //convert target degrees to radians
+  target_degrees *= 0.01745; //degrees*pi/180
+  //radians rotated
+  float r = 0;
+  
+  //accuracy of rotation
+  float acc = 0.02;
+
+  //motor speed 0-255
+  float speed = 100;
+
+  //time step in milliseconds
+  unsigned int step = 5;
+  //time step in seconds
+  float step_time = (float) step/1000.0;
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  while (r-target_degrees > acc || r-target_degrees < -acc){
+    if (target_degrees > r){
+      setRightMotor(speed);
+      setLeftMotor(-speed);
+    }
+    if (target_degrees < r){
+      //rotate 
+      setRightMotor(-speed);
+      setLeftMotor(speed);
+    }
+    delay(step);
+    mpu.getEvent(&a, &g, &temp);
+    delay(step);
+    r += g.gyro.z * step_time*2;
+  }
+  //stop motors
+  setRightMotor(0);
+  setLeftMotor(0);
+}
+
+void setup(void) {
+  //motor setup
+  ledcSetup(M1_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M1_IN_2_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_2_CHANNEL, freq, resolution);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+
+  pinMode(M1_I_SENSE, INPUT);
+  pinMode(M2_I_SENSE, INPUT);
+
+  //set right motor to stop
+  ledcWrite(M2_IN_2_CHANNEL, 0);
+  
+  // Serial.begin(115200);
+  // while (!Serial)
+  //   delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  // Serial.println("Adafruit MPU6050 test!");
 
   pinMode(ADC_1_CS, OUTPUT);
   pinMode(ADC_2_CS, OUTPUT);
@@ -31,100 +147,103 @@ void setup(void) {
 
   // Try to initialize!
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
+    //Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
     }
   }
-  Serial.println("MPU6050 Found!");
+  //Serial.println("MPU6050 Found!");
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+  // Serial.print("Accelerometer range set to: ");
+  // switch (mpu.getAccelerometerRange()) {
+  // case MPU6050_RANGE_2_G:
+  //   Serial.println("+-2G");
+  //   break;
+  // case MPU6050_RANGE_4_G:
+  //   Serial.println("+-4G");
+  //   break;
+  // case MPU6050_RANGE_8_G:
+  //   Serial.println("+-8G");
+  //   break;
+  // case MPU6050_RANGE_16_G:
+  //   Serial.println("+-16G");
+  //   break;
+  // }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
+  // Serial.print("Gyro range set to: ");
+  // switch (mpu.getGyroRange()) {
+  // case MPU6050_RANGE_250_DEG:
+  //   Serial.println("+- 250 deg/s");
+  //   break;
+  // case MPU6050_RANGE_500_DEG:
+  //   Serial.println("+- 500 deg/s");
+  //   break;
+  // case MPU6050_RANGE_1000_DEG:
+  //   Serial.println("+- 1000 deg/s");
+  //   break;
+  // case MPU6050_RANGE_2000_DEG:
+  //   Serial.println("+- 2000 deg/s");
+  //   break;
+  // }
 
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+  // Serial.print("Filter bandwidth set to: ");
+  // switch (mpu.getFilterBandwidth()) {
+  // case MPU6050_BAND_260_HZ:
+  //   Serial.println("260 Hz");
+  //   break;
+  // case MPU6050_BAND_184_HZ:
+  //   Serial.println("184 Hz");
+  //   break;
+  // case MPU6050_BAND_94_HZ:
+  //   Serial.println("94 Hz");
+  //   break;
+  // case MPU6050_BAND_44_HZ:
+  //   Serial.println("44 Hz");
+  //   break;
+  // case MPU6050_BAND_21_HZ:
+  //   Serial.println("21 Hz");
+  //   break;
+  // case MPU6050_BAND_10_HZ:
+  //   Serial.println("10 Hz");
+  //   break;
+  // case MPU6050_BAND_5_HZ:
+  //   Serial.println("5 Hz");
+  //   break;
+  // }
 
-  Serial.println("");
+  // Serial.println("");
   delay(100);
 }
 
 void loop() {
+  delay(1000);
+  rotate(90);
+
 
   /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  // sensors_event_t a, g, temp;
+  // mpu.getEvent(&a, &g, &temp);
 
   /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.print(" m/s^2 \t");
+  // Serial.print("Acceleration X: ");
+  // Serial.print(a.acceleration.x);
+  // Serial.print(", Y: ");
+  // Serial.print(a.acceleration.y);
+  // Serial.print(", Z: ");
+  // Serial.print(a.acceleration.z);
+  // Serial.print(" m/s^2 \t");
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.print(" rad/s \t");
+  // Serial.print("Rotation X: ");
+  // Serial.print(g.gyro.x);
+  // Serial.print(", Y: ");
+  // Serial.print(g.gyro.y);
+  // Serial.print(", Z: ");
+  // Serial.print(g.gyro.z);
+  // Serial.print(" rad/s \t");
 
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
+  // Serial.print("Temperature: ");
+  // Serial.print(temp.temperature);
+  // Serial.println(" degC");
 }
