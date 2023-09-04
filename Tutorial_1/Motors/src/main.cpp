a0227fd8c74cd7b10ccce8c2f792507684715241
Shenzhi Shi <s.shenzhi42@gmail.com>
2023-09-04 13:44:02 -0400

    motors

--- Tutorial_1/Motors/src/main.cpp
+++ Tutorial_1/Motors/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
-const unsigned int M1_IN_1;
-const unsigned int M1_IN_2;
-const unsigned int M2_IN_1;
-const unsigned int M2_IN_2;
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
 
-const unsigned int M1_IN_1_CHANNEL;
-const unsigned int M1_IN_2_CHANNEL;
-const unsigned int M2_IN_1_CHANNEL;
-const unsigned int M2_IN_2_CHANNEL;
+const unsigned int M1_IN_1_CHANNEL = 0;
+const unsigned int M1_IN_2_CHANNEL = 1;
+const unsigned int M2_IN_1_CHANNEL = 2;
+const unsigned int M2_IN_2_CHANNEL = 3;
 
 const unsigned int M1_I_SENSE = 35;
 const unsigned int M2_I_SENSE = 34;
@@ -16,7 +16,7 @@ const unsigned int M2_I_SENSE = 34;
 const unsigned int PWM_VALUE = 512; // Do not give max PWM. Robot will move fast
 
 const int freq = 5000;
-const int resolution = 10;
+const int resolution = 8;
 
 
 void setup() {
@@ -34,8 +34,68 @@ void setup() {
 
   pinMode(M1_I_SENSE, INPUT);
   pinMode(M2_I_SENSE, INPUT);
+
+  //set right motor to stop
+  ledcWrite(M2_IN_2_CHANNEL, 0);
+}
+
+//positive value = forwards, negative value = backwards, 0 value = brake
+void left(float value){
+  if (value == 0){
+    ledcWrite(M2_IN_1_CHANNEL, 0);
+    ledcWrite(M2_IN_2_CHANNEL, 0);
+    return;
+  }
+  if (value > 0){
+    if (value > 255.0){
+      value = 255.0;
+    }
+    ledcWrite(M2_IN_1_CHANNEL, 0);
+    ledcWrite(M2_IN_2_CHANNEL, (uint32_t) value);
+  }
+  if (value < 0){
+    if (value < -255.0){
+      value = -255.0;
+    }
+    ledcWrite(M2_IN_1_CHANNEL, (uint32_t) -value);
+    ledcWrite(M2_IN_2_CHANNEL, 0);
+  }
+}
+
+void right(float value){
+  if (value == 0){
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+    return;
+  }
+  if (value > 0){
+    if (value > 255.0){
+      value = 255.0;
+    }
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, (uint32_t) value);
+  }
+  if (value < 0){
+    if (value < -255.0){
+      value = -255.0;
+    }
+    ledcWrite(M1_IN_1_CHANNEL, (uint32_t) -value);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+  }
 }
 
 void loop() {
-
+  //alternate forwards and backwards
+  left(100);
+  right(100);
+  delay(1000);
+  left(0);
+  right(0);
+  delay(1000);
+  left(-100);
+  right(-100);
+  delay(1000);
+  left(0);
+  right(0);
+  delay(1000);
 }
