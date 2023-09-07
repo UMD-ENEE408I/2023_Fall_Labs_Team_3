c2d2da2f10658a783894a5d64d45aed682d29541
Shenzhi Shi <s.shenzhi42@gmail.com>
2023-09-07 11:16:10 -0400

    Motor_Encoder

--- Tutorial_1/Motors_Encoders/src/main.cpp
+++ Tutorial_1/Motors_Encoders/src/main.cpp
@@ -24,6 +24,8 @@ const unsigned int PWM_VALUE = 512; // Max PWM given 8 bit resolution
 const int freq = 5000;
 const int resolution = 8;
 
+boolean status = true;
+
 void setup() {
   // Stop the right motor by setting pin 14 low
   // this pin floats high or is pulled
@@ -48,78 +50,106 @@ void setup() {
 
 }
 
-//print encoder counts
-void printEnc(){
-  Encoder enc1(M1_ENC_A, M1_ENC_B);
-  Encoder enc2(M2_ENC_A, M2_ENC_B);
-  while(true) {
-    delay(500);
-    Serial.print("Encoder 1 Value: ");
-    Serial.print(enc1.read());
-    Serial.print("  Encoder 2 Value: ");
-    Serial.println(enc2.read());
-  }
-}
-
-//rotate the wheels forward 360◦, stop for 5 seconds and rotate backwards 360◦
-void rotateWheels(){
-  //make sure motor is stopped
-  ledcWrite(M1_IN_1_CHANNEL, 0);
-  ledcWrite(M1_IN_2_CHANNEL, 0);
-  ledcWrite(M2_IN_1_CHANNEL, 0);
-  ledcWrite(M2_IN_2_CHANNEL, 0);
-
-  //create encoder objects
-  Encoder enc1(M1_ENC_A, M1_ENC_B);
-  Encoder enc2(M2_ENC_A, M2_ENC_B);
-  
-  //rotate wheels forwards
+void forward() {
   ledcWrite(M1_IN_1_CHANNEL, 0);
   ledcWrite(M1_IN_2_CHANNEL, 100);
   ledcWrite(M2_IN_1_CHANNEL, 0);
   ledcWrite(M2_IN_2_CHANNEL, 100);
+}
 
-  //check that both wheels rotated 360 degree forwards
-  while(enc1.read()<360 || enc2.read()>-360){
-    Serial.print(enc1.read());
-    Serial.print("//");
-    Serial.println(enc2.read());
-    delay(10);
-  }
-
-  //stop motors
-  ledcWrite(M1_IN_1_CHANNEL, 0);
-  ledcWrite(M1_IN_2_CHANNEL, 0);
-  ledcWrite(M2_IN_1_CHANNEL, 0);
-  ledcWrite(M2_IN_2_CHANNEL, 0);
-
-  //wait 5 seconds
-  delay(5000);
-
+void backward() {
   //rotate wheels backwards
   ledcWrite(M1_IN_1_CHANNEL, 100);
   ledcWrite(M1_IN_2_CHANNEL, 0);
   ledcWrite(M2_IN_1_CHANNEL, 100);
   ledcWrite(M2_IN_2_CHANNEL, 0);
+}
 
-  //check that both wheels rotated 360 degree forwards
-  while(enc1.read()>0 || enc2.read()<0){
-    delay(10);
-  }
-
+void stop() {
   //stop motors
-  ledcWrite(M1_IN_1_CHANNEL, 0);
-  ledcWrite(M1_IN_2_CHANNEL, 0);
-  ledcWrite(M2_IN_1_CHANNEL, 0);
-  ledcWrite(M2_IN_2_CHANNEL, 0);
+  ledcWrite(M1_IN_1_CHANNEL, 512);
+  ledcWrite(M1_IN_2_CHANNEL, 512);
+  ledcWrite(M2_IN_1_CHANNEL, 512);
+  ledcWrite(M2_IN_2_CHANNEL, 512);
+}
+//rotate the wheels forward 360◦, stop for 5 seconds and rotate backwards 360◦
+void rotateWheels(Encoder &enc1, Encoder &enc2){
+
+  if (status) {
+    forward();
+
+    if(enc1.read() > 360) {
+      enc1.write(0);
+      enc2.write(0);
+      stop();
+      delay(5000);
+      status = false;
+    }
+  } else {
+
+    backward();
+
+    if(enc1.read() < -360){
+      enc1.write(0);
+      enc2.write(0);
+      stop();
+      delay(5000);
+      status = true;
+    }
+  }
+}
+
+//set left motor pwm
+void setLeftMtr(float value){
+  if (value == 0){
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+    return;
+  }
+  if (value > 0){
+    if (value > 255.0){
+      value = 255.0;
+    }
+    ledcWrite(M1_IN_1_CHANNEL, 0);
+    ledcWrite(M1_IN_2_CHANNEL, (uint32_t) value);
+  }
+  if (value < 0){
+    if (value < -255.0){
+      value = -255.0;
+    }
+    ledcWrite(M1_IN_1_CHANNEL, (uint32_t) -value);
+    ledcWrite(M1_IN_2_CHANNEL, 0);
+  }
+}
+
+//set left motor speed
+void setLeftSpd(float target_spd, Encoder &enc){
+  float spd = 0; //encoder counts per second
+  float pwm = 0; //motor pwm
+  float thrshld = 1; //threshold for speed
+  int step = 10; //delay in ms for detecting speed
+
+  while(target_spd-spd > thrshld || target_spd-spd < -thrshld){
+    //get speed
+    spd = enc.read();
+    delay(step);
+    spd -= enc.read();
+    spd *= 1000/step; //multiply to get counts/s
+
+    //determine difference in speed
+    if(spd < target_spd){
+      //make motor faster
+
+    }
+  }
+  
 }
 
 void loop() {
   // Create the encoder objects after the motor has
   // stopped, else some sort exception is triggered
-  // Encoder enc1(M1_ENC_A, M1_ENC_B);
-  // Encoder enc2(M2_ENC_A, M2_ENC_B);
-
+  Encoder enc1(M1_ENC_A, M1_ENC_B);
+  Encoder enc2(M2_ENC_A, M2_ENC_B);
   // while(true) {
   //   delay(500);
   //   Serial.print("Encoder 1 Value: ");
@@ -127,6 +157,8 @@ void loop() {
   //   Serial.print("  Encoder 2 Value: ");
   //   Serial.println(enc2.read());
   // }
-  //printEnc();
-  rotateWheels();
+  while(true){
+    rotateWheels(enc1, enc2);
+  } 
+
 }
