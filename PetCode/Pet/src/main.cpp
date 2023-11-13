462f40238fd4886ce0c95652f7db2505d969d8e2
Shenzhi Shi <s.shenzhi42@gmail.com>
2023-11-13 11:10:32 -0500

    speak

---
+++ PetCode/Pet/src/main.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+#include <Adafruit_MCP3008.h>
+#include <Adafruit_MPU6050.h>
+#include <Encoder.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+const unsigned int BUZZ = 26; // Check schematic and see pin connection to buzzer
+const unsigned int BUZZ_CHANNEL = 0; //Selecting PWM channel 0
+
+
+int status = WL_IDLE_STATUS;
+char ssid[] = "AndroidAP79C6"; //  your network SSID (name)
+char pass[] = "enee408i";
+
+unsigned int localPort = 2390;      // local port to listen on
+
+char packetBuffer[255]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
+
+WiFiUDP Udp;
+
+void speak(){
+  ledcWriteNote(BUZZ_CHANNEL, NOTE_C, 8);
+  delay(500);
+  ledcWrite(BUZZ_CHANNEL, 0);
+}
+
+void setup() {
+   // Stop the right motor by setting pin 14 low
+  // this pin floats high or is pulled
+  // high during the bootloader phase for some reason
+  pinMode(14, OUTPUT);
+  digitalWrite(14, LOW);
+  delay(100);
+
+  ledcAttachPin(BUZZ, BUZZ_CHANNEL);
+
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+
+  Udp.begin(localPort);
+}
+
+void loop() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+
+    Serial.print("Received packet of size ");
+
+    Serial.println(packetSize);
+
+    Serial.print("From ");
+
+    IPAddress remoteIp = Udp.remoteIP();
+
+    Serial.print(remoteIp);
+
+    Serial.print(", port ");
+
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) {
+
+      packetBuffer[len] = 0;
+
+    }
+    Serial.print("message: ");
+    Serial.println(packetBuffer[0]);
+    switch(packetBuffer[0]){
+      case 's':
+        speak();
+        Serial.println("spoken");
+        break;
+    }
+
+    // // send a reply, to the IP address and port that sent us the packet we received
+
+    // Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+
+    // Udp.printf("%s", ReplyBuffer);
+
+    // Udp.endPacket();
+  }
+}
